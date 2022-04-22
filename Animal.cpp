9432b5784f3a7b05e37dff4c7d8b999589ee0a45
ashtencodes <ashakemoto@gmail.com>
2022-04-21 15:55:43 -1000

    created animal class implementation

--- Animal.cpp
+++ Animal.cpp
@@ -10,3 +10,90 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include "Animal.h"
+
+const std::string Animal::KINGDOM_NAME = "Animalia";
+
+Animal::Animal(const Weight::t_weight newMaxWeight, const std::string &newClassification,
+               const std::string &newSpecies) {
+    weight.setMaxWeight( newMaxWeight );
+    classification = newClassification;
+    species = newSpecies;
+}
+
+Animal::Animal(const Gender newGender, const Weight::t_weight newWeight, const Weight::t_weight newMaxWeight,
+               const std::string &newClassification, const std::string &newSpecies) {
+    gender = newGender;
+    weight.setWeight( newWeight );
+    weight.setMaxWeight( newMaxWeight );
+    classification = newClassification;
+    species = newSpecies;
+}
+
+std::string Animal::getKingdom() const noexcept {
+    return KINGDOM_NAME;
+}
+
+std::string Animal::getClassification() const noexcept {
+    return classification;
+}
+
+std::string Animal::getSpecies() const noexcept {
+    return species;
+}
+
+Gender Animal::getGender() const noexcept {
+    return gender;
+}
+
+Weight::t_weight Animal::getWeight() const noexcept {
+    return weight.getWeight();
+}
+
+void Animal::setWeight(const Weight::t_weight newWeight) {
+    weight.setWeight( newWeight );
+}
+
+void Animal::setGender(const Gender newGender) {
+    if(gender != Gender::UNKNOWN_GENDER){
+        throw std::logic_error("Can't change gender twice");
+    }
+
+    assert(gender == Gender::UNKNOWN_GENDER);
+    gender = newGender;
+    assert( validate() );
+}
+
+void Animal::dump() const noexcept {
+    FORMAT_LINE_FOR_DUMP( "Animal", "this" ) << this << std::endl;
+    FORMAT_LINE_FOR_DUMP( "Animal", "kingdom" ) << getKingdom() << std::endl;
+    FORMAT_LINE_FOR_DUMP( "Animal", "classification" ) << getClassification() << std::endl;
+    FORMAT_LINE_FOR_DUMP( "Animal", "species" ) << getSpecies() << std::endl;
+    FORMAT_LINE_FOR_DUMP( "Animal", "gender" ) << getGender() << std::endl;
+    FORMAT_LINE_FOR_DUMP( "Animal", "weight" ) << getWeight() << std::endl;
+}
+
+bool Animal::validateClassification(const std::string &checkClassification) noexcept {
+    if( checkClassification.empty() ){
+        std::cout << PROGRAM_NAME << ": classification cannot be empty!" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool Animal::validateSpecies(const std::string &checkSpecies) noexcept {
+    if( checkSpecies.empty() ){
+        std::cout << PROGRAM_NAME << ": species cannot be empty!" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool Animal::validate() const noexcept {
+    assert( Node::validate() );
+    assert( !getKingdom().empty );
+    assert( validateClassification( getClassification() ) );
+    assert( validateSpecies( getSpecies() ) );
+
+    assert( weight.vaidate() );
+    return true;
+}
--- Animal.h
+++ Animal.h
@@ -10,7 +10,38 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #pragma once
+#include <string>
+#include "Node.h"
+#include "Gender.h"
+#include "Weight.h"
 
-class Animal {
+class Animal : public Node {
 
+private:
+    std::string species;
+    std::string classification;
+    Gender gender;
+    Weight weight;
+
+public:
+    static const std::string KINGDOM_NAME;
+
+    Animal( const Weight::t_weight newMaxWeight, const std::string &newClassification, const std::string &newSpecies );
+    Animal( const Gender newGender, const Weight::t_weight newWeight, const Weight::t_weight newMaxWeight, const std::string &newClassification, const std::string &newSpecies );
+
+public:
+    std::string getKingdom() const noexcept;
+    std::string getClassification() const noexcept;
+    std::string getSpecies() const noexcept;
+    Gender getGender() const noexcept;
+    Weight::t_weight getWeight() const noexcept;
+
+    void setWeight( const Weight::t_weight newWeight );
+    virtual std::string speak() const noexcept=0;
+    void dump() const noexcept override;
+    bool validate() const noexcept override;
+
+    static bool validateClassification( const std::string &checkClassification ) noexcept;
+    static bool validateSpecies( const std::string &checkSpecies ) noexcept;
+    void setGender( const Gender newGender );
 };
