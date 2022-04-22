a61ba9b6a73461869ffae5ef55db8a2ad39d7d44
ashtencodes <ashakemoto@gmail.com>
2022-04-21 15:21:33 -1000

    created animal class shell

---
+++ Animal.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-Animal-Farm - EE 205 - Spr 2022
+///
+/// @file Animal.h
+/// @version 1.0
+///
+/// @author @Ashten Akemoto <aakemoto@hawaii.edu>
+/// @date   21_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class Animal {
+
+};
